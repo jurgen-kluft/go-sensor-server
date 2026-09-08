585b91ad5edc2e9e0a36ff0429b894220f24d032
jurgen.kluft <jurgen.kluft@gmail.com>
2026-09-08 21:09:20 +0800

    * go-sensor-server
    + themes

---
+++ sensor-server/house_themes.go
@@ -0,0 +1,7 @@
+package sensorserver
+
+// A theme is a collection of lights and a global setting for those lights.
+// For example, a "Movie Night" theme might dim the lights in the living
+// room and set them to a warm color temperature.
+
+// A Christmas theme might set the lights to red and green colors.
